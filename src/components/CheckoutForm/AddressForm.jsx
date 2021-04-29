f117a46fdbd783d73581a6171912839d28a51a15
Hassan-Ib <azanebrahim@gmail.com>
2021-04-28 18:31:20 -0600

    fixed : typo

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  InputLabel,
+  Button,
+  Grid,
+  Typography,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
 
 const AddressForm = () => {
-  return <div>address form</div>;
+  const methods = useForm();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping Address
+      </Typography>
+      <FormProvider {...methods}>
+        <form action="" onSubmit={}>
+          <Grid container spacing={3}></Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
